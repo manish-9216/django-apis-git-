50732a2ebb9727503acefce8b0fd3c72cc957f61
manish-9216 <aadi360rr@gmail.com>
2022-08-23 11:23:22 +0530

    first commit

---
+++ django crud api/crud/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import CartItemViews
+
+
+
+urlpatterns = [
+    path('cart-items/', CartItemViews.as_view()),
+    path('cart-items/<int:id>', CartItemViews.as_view())
+
+
+]
